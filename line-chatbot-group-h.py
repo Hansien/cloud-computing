1440ae26d388b01dce0233b1d0434129b0336c7a
buzhiliao <hansien@live.com>
2020-03-16 19:53:51 +0800

    test for message type

--- line-chatbot-group-h.py
+++ line-chatbot-group-h.py
@@ -84,11 +84,11 @@ def callback():
 
 # Handler function for Text Message
 def handle_TextMessage(event):
-    if redis1.get(event.message.text) == None:
-        msg = 'No Rusult, you can type "help" to get a list of commands!' 
-    else:
+    # if redis1.get(event.message.text) == None:
+    #     msg = 'No Rusult, you can type "help" to get a list of commands!' 
+    # else:
         # query_t = str(event.message.text)
-        msg = str(type(event.message.text))
+    msg = event.message.text.lower()
         # msg = redis1.get(query_t.lower()).decode()
     line_bot_api.reply_message(
         event.reply_token,
