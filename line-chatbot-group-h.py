dba6673767081d423a8d86dd7ca79c26ae30d18b
buzhiliao <hansien@live.com>
2020-04-15 18:38:25 +0800

    fix bugs

--- line-chatbot-group-h.py
+++ line-chatbot-group-h.py
@@ -119,7 +119,7 @@ def handle_TextMessage(event):
     # Case-insensitive full keyword matching
     if redis1.get(event.message.text.lower()) == None:
         msg = 'No Rusult, you can type "help" to get a list of commands!'
-    else if redis1.get(event.message.text.lower()) == "CH":
+    elif redis1.get(event.message.text.lower()) == "CH":
         lanFlag = CH
     else:
         msg = redis1.get(event.message.text.lower()).decode()
